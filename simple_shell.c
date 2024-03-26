a0a24807e0f5609c98690da6978c0149b84baf6d
Scoob1 <tamara_walling@yahoo.com>
2024-03-26 15:56:47 +0000

    simple shell betty is mad

--- simple_shell.c
+++ simple_shell.c
@@ -3,4 +3,32 @@
 #include <unistd.h>
 #include <string.h>
 
+#define BUFFER_SIZE 1024
 
+int main(void)
+{
+	char buffer[BUFFER_SIZE];
+
+	while (1)
+	{
+		printf("#cisfun$ ")
+				if (!fgets(buffer, BUFFER_SIZE, stdin))
+       	{
+		printf("\n");
+		break;
+	}
+	buffer[strcspn(buffer, "\n")] = '\0';
+
+	if (fork() == 0)
+       	{
+		execlp(buffer, buffer, NULL);
+		perror(buffer);
+		exit(exit_failure);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
+return EXIT_SUCCESS;
+}
