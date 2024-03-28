45d7955e4db382585dab5fd0c7ebfc3398bc6d52
root <tamara_walling@yahoo.com>
2024-03-28 03:48:47 +0000

    fixing code and errrors for task 2

---
+++ simple_shell.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define BUFSIZE 1024
+
+int main(void)
+{
+	char cmd[BUFSIZE];
+	char *args[2];
+
+	while (1)
+	{
+		printf("#cisfun$");
+
+		if (!fgets(cmd, BUFSIZE, stdin))
+		{
+			printf("\n");
+			break;
+		}
+
+		cmd[strcspn(cmd, "\n")] = '\0';
+
+		args[0] = cmd;
+		args[1] = NULL;
+
+		if (execve(cmd, args, NULL) == -1)
+		{
+			printf("%s: command not found\n", cmd);
+		}
+	}
+
+	return (0);
+}
