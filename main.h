edd2d0461f71f2dec7da84118824c86ef517f358
Scoob1 <tamara_walling@yahoo.com>
2024-04-01 18:40:02 +0000

    edited main file

--- main.h
+++ main.h
@@ -6,10 +6,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
 #define BUFSIZE 1024
 #define MAX_ARGS 32
 
 void tokenize_input(char *buffer, char *args[]);
+int execute_command(char *args[], int num_args);
 
 #endif
